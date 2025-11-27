11aa503e3ec4dfbaa1d9ceff4bd1f27a358f40ba
ANIRUDH <minatisahoo266@gmail.com>
2025-11-27 23:53:38 +0530

    Update ultimatepyramid.c

--- ultimatepyramid.c
+++ ultimatepyramid.c
@@ -2,25 +2,26 @@
 int main()
 {
 	int i,j,k;
-	for(i=1;i<=5;i++){
-	   	for(k=1;k<=5-i;k++){
+	for(i=1;i<=5;i++){  //here we have done the part of increase of stars from 1 to 5
+	   	for(k=1;k<=5-i;k++){  //here we have done the part of spaces 
 	   		printf(" ");
 		   }
-	   	for(j=1;j<=i;j++){
+	   	for(j=1;j<=i;j++){    //here we have done that part of increasing printing stars
 	   		printf(" *");
 		   }
 		   printf("\n");
 	   }
 	   
-	   for(i=5;i>=1;i--){
-	   	for(k=1;k<=5-i;k++){
+	   for(i=5;i>=1;i--){  //here we have done that decrease of stars from 1 to 5
+	   	for(k=1;k<=5-i;k++){//here we have done the part of spaces
 	   		printf(" ");
 		   }
 		   
-	   	for(j=1;j<=i;j++){
+	   	for(j=1;j<=i;j++){ //her we have done that part of decreasinfg stars
 	   		printf(" *");
 		   }
 		   printf("\n");
 	   }
 	   return 0;
 }
+
